206ac2c2214aae55c836cbbd3d774422959e6a02
Charles Ha <behappy@plusdica.com>
2016-10-01 21:31:04 -0400

    Added file resource to testserver.

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,9 +6,13 @@ node 'losttemple.linuxmaster.com' {
 #	include vim_upgrade
 #}
 
+node 'vm01.linuxmaster.com' {
+	file { '/tmp/testserver' :
+		content => "This is a test server.\n",
+	}
 
 node 'vm01.linuxmaster.com' {
-	include vim_upgrade
-	include sshd
+#	include vim_upgrade
+#	include sshd
 }
 
